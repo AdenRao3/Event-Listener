45844afc4f4ec81818a3190d3ce0838b0c3e541b
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-02-15 13:19:05 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 --
 -- main.lua
 --
---This program puts a colored background on the screen and an interactive image
+--This program puts a colored background on the screen and an image which prints something to the console when you touch it and it also print text on screen. Also my name is 
 --
 --Created By: Aden Rao
 --Created On: 2019/02/13
@@ -14,10 +14,13 @@ local soccerBall = display.newImageRect( "assets/ball.png", 250, 250 )
 soccerBall.x = 165
 soccerBall.y = 250
 soccerBall.id = "soccer ball"
- 
+
+local myText = display.newText( "Aden Rao", 160, 100, native.systemFont, 50 )
+
 local function onObjectTouch( event )
     if ( event.phase == "began" ) then
-        print( "You touched the soccer ball" )
+        print( "You touched the ball")
+        local myText = display.newText( "You touched the ball", 160, 375, native.systemFont, 25 )
     end
     return true
 end
